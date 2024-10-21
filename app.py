d4d4932795589be3458eeb98594e275b40c1887d
Isaac Wismer <iwismer@squareup.com>
2024-10-21 11:22:01 -0400

    Allow multiple calendars to be served

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Serve new combined calendar
 
 import os
 import json
-from flask import Flask
+from flask import Flask, make_response
 from combine_calendars import ExistingCalendar, generate_combined_calendar
 
 app = Flask(__name__)
@@ -13,17 +13,29 @@ cwd = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(cwd, 'config.json')) as f:
     config = json.load(f)
 
-existing_calendars = []
+served_calendars = {}
 for calendar in config['calendars']:
-    existing_calendar = ExistingCalendar(calendar['name'],
-                                         calendar['description'],
-                                         calendar['url'])
-    existing_calendars.append(existing_calendar)
+    existing_calendars = []
+    for sub_cal in calendar['calendars']:
+        existing_calendar = ExistingCalendar(sub_cal['name'],
+                                            sub_cal['description'],
+                                            sub_cal['url'])
+        existing_calendars.append(existing_calendar)
+    served_calendars[calendar['name']] = existing_calendars
 
+@app.route('/')
+def empty_response():
+    response = make_response('', 200)
+    return response
 
-@app.route("/calendar")
-def combine_calendar():
+@app.route("/calendar/<cal_name>")
+def combine_calendar(cal_name):
     """
     Return combined calendar in ics format
     """
-    return generate_combined_calendar(config['name'], existing_calendars).serialize()
+    if cal_name not in served_calendars:
+        return make_response("Not Found", 404)
+    response =  make_response(generate_combined_calendar(cal_name, served_calendars[cal_name]).serialize())
+    response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
+    response.headers['Content-Type'] = 'text/calendar'
+    return response
